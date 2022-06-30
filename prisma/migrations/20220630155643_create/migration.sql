187c0c6dc31651bac8955ad40179f01e99785af2
stormsidali2001 <assoulsidali@gmail.com>
2022-06-30 18:08:30 +0200

    relation role-user

---
+++ prisma/migrations/20220630155643_create/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `test` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `test`;
+
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Role_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/migrations/20220630160008_update_role/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `role` ADD PRIMARY KEY (`name`);
