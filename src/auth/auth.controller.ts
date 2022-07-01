def27ab9c8553f7c339b55099a1bdd6311b24d7e
stormsidali2001 <assoulsidali@gmail.com>
2022-07-01 19:50:41 +0200

    signUpDeliveryMan

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { Public } from "src/common/decorators/public.decorator";
-import { ClientRegistrationDto } from "src/dto/registrationDtos";
+import { AdminRegistrationDto, ClientRegistrationDto, DeliveryManRegistrationDto } from "src/dto/registrationDtos";
 import { AuthService } from "./auth.service";
 
 
-@Controller('')
+@Controller('auth')
 export class AuthController{
     constructor(
         private readonly authService:AuthService
@@ -14,5 +14,17 @@ export class AuthController{
     @Post('signup/client')
     async signUpClient(@Body() client:ClientRegistrationDto){
         return this.authService.signUpClient(client);
+    }
+
+    @Public()
+    @Post('signup/deliveryMan')
+    async signUpDeliveryMan(@Body() deliveryMan:DeliveryManRegistrationDto){
+        return this.authService.signUpDeliveryMan(deliveryMan);
+    }
+    
+    @Public()
+    @Post('signup/admin/saffsa;sfajfsajfsajl;fsfsa;sfkj')
+    async signUpAdmin(@Body() admin:AdminRegistrationDto){
+        return this.authService.signUpAdmin(admin);
     }
 }
